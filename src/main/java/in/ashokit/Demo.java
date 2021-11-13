5827cba295e14ab10ef4d234b3a2110c23d7f2e3
Chenna IT <chennakesava0607@gmail.com>
2021-11-13 09:36:10 +0530

    added constructor

--- src/main/java/in/ashokit/Demo.java
+++ src/main/java/in/ashokit/Demo.java
@@ -1,5 +1,12 @@
 package in.ashokit;
 
+<<<<<<< Updated upstream
 public class Demo extends RuntimeException{
 	
+=======
+public class Demo {
+	public void m1(){
+	//logic to Bug-1334
+}
+>>>>>>> Stashed changes
 }
