b2d29f19cf2adb4e229e6e53d6397d355a7912a2
Chenna IT <chennakesava0607@gmail.com>
2021-11-13 17:02:13 +0530

    HIS-301 changes

--- src/main/java/in/ashokit/App.java
+++ src/main/java/in/ashokit/App.java
@@ -6,9 +6,14 @@ package in.ashokit;
  */
 public class App 
 {
+	private Map<string, Object> cache=new HashMap<String,Object>(); 
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
 	int i=10;
     }
+	HIS-301 changes
+	private void loadDataToCache(){
+		//logic
+	}
 }
